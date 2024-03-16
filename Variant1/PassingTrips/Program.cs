027cb415c97f612f9e4bb64ed86e6bd963659602
antonborisov571 <antonborisov571@gmail.com>
2024-03-16 16:21:19 +0300

    Added project

---
+++ Variant1/PassingTrips/Program.cs
@@ -0,0 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using PassingTrips;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AppDbContext>(optionsBuilder =>
+    optionsBuilder.UseNpgsql(builder.Configuration.GetConnectionString("TripsDatabase"))
+        .UseSnakeCaseNamingConvention());
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapControllers();
+
+app.UseHttpsRedirection();
+
+app.Run();
