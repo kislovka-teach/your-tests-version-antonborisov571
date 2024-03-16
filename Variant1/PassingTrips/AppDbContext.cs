2e037f3926604de69a647c8bbdafcd17d26bd778
antonborisov571 <antonborisov571@gmail.com>
2024-03-17 00:15:32 +0300

    Added new objects

--- Variant1/PassingTrips/AppDbContext.cs
+++ Variant1/PassingTrips/AppDbContext.cs
@@ -21,5 +21,39 @@ public class AppDbContext : DbContext
         base.OnModelCreating(modelBuilder);
         modelBuilder.ApplyConfiguration(new UserEntityConfiguration());
         modelBuilder.ApplyConfiguration(new TripEntityConfiguration());
+
+        modelBuilder.Entity<User>().HasData(new User
+        {
+            Id = 1,
+            FirstName = "Вася",
+            LastName = "Пупкин",
+            Login = "login123",
+            Password = "qwerty1!Q",
+            DateRegistration = default,
+            NumberPhone = "1234567890",
+            DrivingExperience = 1,
+        });
+
+        modelBuilder.Entity<City>().HasData(new List<City>
+        {
+            new()
+            {
+                Id = 1,
+                Name = "Москва",
+                CoordinateId = 1
+            },
+            new()
+            {
+                Id = 2,
+                Name = "Казань",
+                CoordinateId = 2
+            }
+        });
+
+        modelBuilder.Entity<Coordinate>().HasData(new List<Coordinate>
+        {
+            new() { Id = 1, Width = 0, Longitude = 0 },
+            new() { Id = 2, Width = 50, Longitude = 0 }
+        });
     }
 }
