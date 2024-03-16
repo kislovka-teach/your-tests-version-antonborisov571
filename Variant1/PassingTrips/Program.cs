fe9b170422cb2dae3568d84b6a147801a3ffb8ed
antonborisov571 <antonborisov571@gmail.com>
2024-03-16 21:25:23 +0300

    Added dependency in services

--- Variant1/PassingTrips/Program.cs
+++ Variant1/PassingTrips/Program.cs
@@ -1,14 +1,16 @@
 using Microsoft.EntityFrameworkCore;
 using PassingTrips;
+using PassingTrips.Configurations;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
+builder.Services.AddJwtTokens();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AppDbContext>(optionsBuilder =>
     optionsBuilder.UseNpgsql(builder.Configuration.GetConnectionString("TripsDatabase"))
         .UseSnakeCaseNamingConvention());
+builder.Services.AddRepositories();
 builder.Services.AddControllers();
 
 var app = builder.Build();
@@ -18,6 +20,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers();
 
