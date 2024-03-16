d80c979bfba315e2783542e946d1b32fb3985058
antonborisov571 <antonborisov571@gmail.com>
2024-03-16 16:21:59 +0300

    Added models

---
+++ Variant1/PassingTrips/Models/Coordinate.cs
@@ -0,0 +1,8 @@
+﻿namespace PassingTrips.Models;
+
+public class Coordinate
+{
+    public int Id { get; set; }
+    public float Width { get; set; }
+    public float Longitude { get; set; }
+}
---
+++ Variant1/PassingTrips/Models/User.cs
@@ -0,0 +1,15 @@
+﻿namespace PassingTrips.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Login { get; set; }
+    public string Password { get; set; }
+    public DateOnly DateRegistration { get; set; }
+    public string NumberPhone { get; set; }
+    public int DrivingExperience { get; set; }
+    public List<Trip> DriverTrips { get; set; }
+    public List<Trip> PassengerTrips { get; set; }
+}
