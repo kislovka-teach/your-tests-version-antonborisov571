a3dd6eadeb3cbaaa1fea36c3dcdc79d879c9c8aa
antonborisov571 <antonborisov571@gmail.com>
2024-03-17 00:15:56 +0300

    Added for swagger authorization

--- Variant1/PassingTrips/Program.cs
+++ Variant1/PassingTrips/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using PassingTrips;
 using PassingTrips.Configurations;
 
@@ -6,11 +7,38 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddJwtTokens();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(opt =>
+{
+    opt.SwaggerDoc("v1", new OpenApiInfo { Title = "MyAPI", Version = "v1" });
+    opt.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "bearer"
+    });
+    opt.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type=ReferenceType.SecurityScheme,
+                    Id="Bearer"
+                }
+            },
+            new string[]{}
+        }
+    });
+});
 builder.Services.AddDbContext<AppDbContext>(optionsBuilder =>
     optionsBuilder.UseNpgsql(builder.Configuration.GetConnectionString("TripsDatabase"))
         .UseSnakeCaseNamingConvention());
 builder.Services.AddRepositories();
+builder.Services.AddServices();
 builder.Services.AddControllers();
 
 var app = builder.Build();
